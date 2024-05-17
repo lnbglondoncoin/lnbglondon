fc2921c3243fe81343d9759f465ba3f62a79241d
cyhammad <hammad727272@gmail.com>
2024-05-17 11:59:11 +0500

    initial setup

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center">
+      LNBG COIN COMING SOON
+    </main>
+  );
+}
