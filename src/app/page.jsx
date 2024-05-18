83ae04d034da53f8c97adc09ed53ac323a8ac0e3
cyhammad <hammad727272@gmail.com>
2024-05-18 19:50:52 +0500

    empowering section done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import EmpoweringSection from "@/components/EmpoweringSection";
 import FiveCoinsSection from "@/components/FiveCoinsSection";
 import GatewaySection from "@/components/GatewaySection";
 import MainBanner from "@/components/MainBanner";
@@ -8,6 +9,7 @@ export default function Home() {
       <MainBanner />
       <FiveCoinsSection />
       <GatewaySection />
+      <EmpoweringSection />
     </main>
   );
 }
---
+++ src/components/EmpoweringSection.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const EmpoweringSection = () => {
+  return (
+    <section className="flex items-center justify-center text-center">
+      <div className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-center gap-y-10 bg-[url('/bgs/dotted-map.png')] bg-center bg-no-repeat px-5 py-10">
+        <div className="flex gap-x-1">
+          <div className="h-2 w-2 rounded-full bg-primary"></div>
+          <div className="h-2 w-2 rounded-full bg-primary"></div>
+          <div className="h-2 w-2 rounded-full bg-primary"></div>
+          <div className="h-2 w-2 rounded-full bg-primary"></div>
+          <div className="h-2 w-2 rounded-full bg-primary"></div>
+          <div className="h-2 w-2 rounded-full bg-primary"></div>
+          <div className="h-2 w-2 rounded-full bg-primary"></div>
+        </div>
+        <h1 className="text-center text-6xl font-bold leading-[4.5rem] tracking-tighter">
+          Empowering Companies & Empowering You
+          <span className="text-primary">.</span>
+        </h1>
+        <span className="text-2xl tracking-tight text-lightgray">
+          Through tokenization of equity and assets
+        </span>
+        <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div className="group flex max-w-72 cursor-pointer flex-col items-center gap-y-5 text-center">
+            <span className="text-7xl font-bold text-primary">6386</span>
+            <span className="text-2xl font-bold">More leads</span>
+            <span className="text-sm tracking-wide text-gray2">
+              Our platform generates additional web traffic of highly motivated
+              crypto users to each of our partners.
+            </span>
+            <div className="h-1 w-20 rounded-full bg-darkgray transition-all duration-200 ease-in-out group-hover:w-56 group-hover:bg-primary"></div>
+          </div>
+          <div className="group flex max-w-72 cursor-pointer flex-col items-center gap-y-5 text-center">
+            <span className="text-7xl font-bold text-primary">16000</span>
+            <span className="text-2xl font-bold">Higher Sales</span>
+            <span className="text-sm tracking-wide text-gray2">
+              Our platform generates a steady flow of motivated crypto users to
+              our partners.thanks to our dynamic networks.
+            </span>
+            <div className="h-1 w-20 rounded-full bg-darkgray transition-all duration-200 ease-in-out group-hover:w-56 group-hover:bg-primary"></div>
+          </div>
+          <div className="group flex max-w-72 cursor-pointer flex-col items-center gap-y-5 text-center">
+            <span className="text-7xl font-bold text-primary">8327</span>
+            <span className="text-2xl font-bold">Clients worldwide</span>
+            <span className="text-sm tracking-wide text-gray2">
+              Our streamlined automated processes make all the work for you. It
+              is simple and easy solution for anyone.
+            </span>
+            <div className="h-1 w-20 rounded-full bg-darkgray transition-all duration-200 ease-in-out group-hover:w-56 group-hover:bg-primary"></div>
+          </div>
+          <div className="group flex max-w-72 cursor-pointer flex-col items-center gap-y-5 text-center">
+            <span className="text-7xl font-bold text-primary">
+              2000<span className="text-darkgray">+</span>
+            </span>
+            <span className="text-2xl font-bold">More leads</span>
+            <span className="text-sm tracking-wide text-gray2">
+              We validate crypto service providers before listing them to
+              protect our users from widespread crypto frauds and scams.
+            </span>
+            <div className="h-1 w-20 rounded-full bg-darkgray transition-all duration-200 ease-in-out group-hover:w-56 group-hover:bg-primary"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EmpoweringSection;
