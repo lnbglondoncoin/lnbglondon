f0a42c5c440d96a45aae8306d1825507d3c9b94a
cyhammad <hammad727272@gmail.com>
2024-08-21 02:23:13 +0500

    buy done

--- src/app/buy/page.jsx
+++ src/app/buy/page.jsx
@@ -1,8 +1,11 @@
 "use client";
 
+import Button from "@/components/buttons/Button";
 import { Vortex } from "@/components/ui/vortex";
+import { Transak } from "@transak/transak-sdk";
 
 export default function BuyPage() {
+  
   return (
     <section className="relative top-0 flex w-full max-w-[100vw] items-center justify-center overflow-hidden bg-coal">
       <Vortex
@@ -11,9 +14,12 @@ export default function BuyPage() {
         rangeY={800}
         particleCount={700}
         baseHue={50}
-        className="flex h-full w-full items-center justify-center pb-10 pt-[120px]"
+        className="flex h-full min-h-screen w-full items-start justify-center pb-10 pt-[120px]"
       >
-        
+        <div className="flex h-[500px] w-full max-w-[400px] flex-col gap-5 rounded-lg bg-ash p-4">
+          <span className="text-xl">Buy Crypto with Transak</span>
+          <Button title="Buy With Transak" onClick={() => launchTransak()} />
+        </div>
       </Vortex>
     </section>
   );
