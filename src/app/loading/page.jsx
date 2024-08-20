9952bb05bec82c5ce39f7b9382c52450f011081b
cyhammad <hammad727272@gmail.com>
2024-08-20 23:55:18 +0500

    loader updated

--- src/app/loading/page.jsx
+++ src/app/loading/page.jsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { MutatingDots } from "react-loader-spinner";
+import { CirclesWithBar, MutatingDots } from "react-loader-spinner";
 
 export default function LoadingPage() {
   return (
-    <div className="absolute inset-0 z-[99999999999] flex flex-col items-center justify-center bg-coal">
-      <MutatingDots
+    <div className="absolute scale-150 inset-0 z-[99999999999] flex flex-col items-center justify-center bg-coal">
+      {/* <MutatingDots
         visible={true}
         height="100"
         width="100"
@@ -15,8 +15,20 @@ export default function LoadingPage() {
         ariaLabel="mutating-dots-loading"
         wrapperStyle={{}}
         wrapperClass=""
+      /> */}
+      <CirclesWithBar
+        height="100"
+        width="100"
+        color="#ffba00"
+        outerCircleColor="#FFFc54"
+        innerCircleColor="#ffba00"
+        barColor="#4fa94d"
+        ariaLabel="circles-with-bar-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
       />
-      <h1 className="text-4xl font-bold">LNBG</h1>
+      {/* <h1 className="text-4xl font-bold">LNBG</h1> */}
     </div>
   );
 }
