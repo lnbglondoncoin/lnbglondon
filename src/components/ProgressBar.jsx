00f0ce8918e2e347f0b82ef12a40cb9f516771e5
cyhammad <hammad727272@gmail.com>
2024-08-25 01:14:33 +0500

    percent added

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,24 +1,26 @@
-const ProgressBar = ({ contractData ,soldPercentage, lang = "en" }) => {
-
+const ProgressBar = ({ contractData, soldPercentage, lang = "en" }) => {
   console.log(soldPercentage, "soldPercentage");
   let integerPart = Math.trunc(soldPercentage);
-  
+
   const roundOff = (num) => {
     // convert string to int
     let number = parseFloat(num);
     // round off to 2 decimal
     return number.toFixed(4);
   };
-  
+
   return (
     <div className="flex w-full flex-col gap-y-1">
       <div className="flex justify-between">
         <h3 className="px-1 font-sans text-sm sm:leading-[18px]">
           Stage 1 / 10
         </h3>
-        <h3 className="px-1 text-sm font-sans sm:leading-[18px]">
-        ${roundOff(contractData?.raisedAmount)} / $300,000
-        </h3>
+        <div className="flex items-center gap-2">
+          <h3 className="px-1 font-sans text-sm sm:leading-[18px]">
+            ${roundOff(contractData?.raisedAmount)} / $300,000
+          </h3>
+          <span className="text-gray2 text-sm">(41.88%)</span>
+        </div>
       </div>
       <div className="h-4 w-full rounded-full border-4 border-ash sm:h-[17px]">
         <div
