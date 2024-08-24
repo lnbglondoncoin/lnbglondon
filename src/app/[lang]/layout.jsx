2d2928d28e622e719aa5fa58600c78e1761b17e3
cyhammad <hammad727272@gmail.com>
2024-08-24 15:58:39 +0500

    cookies banner fixed

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -5,6 +5,27 @@ import React from "react";
 import { ToastContainer } from "react-toastify";
 import CookiesBanner from "@/components/CookiesBanner";
 
+export const metadata = {
+  title: "LNBG London Coin - Secure & Innovative Cryptocurrency",
+  description:
+    "LNBG London Coin is a secure and innovative cryptocurrency inspired by London's financial heritage. Explore decentralized digital finance with our trusted and efficient platform.",
+  image: "/static/logo.png",
+  openGraph: {
+    image: ["/static/logo.png"],
+  },
+  author: "LNBG London Coin Team",
+  keywords: [
+    "LNBG",
+    "London Coin",
+    "Cryptocurrency",
+    "Blockchain",
+    "Digital Currency",
+    "London Crypto",
+    "Decentralized Finance",
+    "Secure Coin",
+  ],
+};
+
 export default function layout({ children, params }) {
   const lang = params.lang;
   return (
