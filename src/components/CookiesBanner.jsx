b623b425944fdc4efda8ec60889a46d25f1c12e7
cyhammad <hammad727272@gmail.com>
2024-08-24 15:57:46 +0500

    cookies banner fixed

--- src/components/CookiesBanner.jsx
+++ src/components/CookiesBanner.jsx
@@ -4,6 +4,7 @@ import { Store } from "@/context/Store/Store";
 import { Cookie } from "lucide-react";
 import { useContext, useState } from "react";
 import Button from "./buttons/Button";
+import Image from "next/image";
 
 export default function CookiesBanner() {
   const { cookiesAccepted, setCookiesAccepted } = useContext(Store);
@@ -14,23 +15,36 @@ export default function CookiesBanner() {
     setIsOpen(false);
   };
   return isOpen ? (
-    <div className="fixed bottom-0 z-50 flex w-screen items-center justify-center bg-ash px-5 py-10">
-      <div className="flex flex-col md:flex-row gap-5 w-full max-w-7xl items-center justify-between">
-        <div className="flex items-center gap-4">
+    <div className="fixed bottom-0 z-50 flex w-screen items-center justify-center bg-ash px-5 py-5 md:py-10">
+      <div className="flex w-full max-w-7xl flex-col items-center justify-between gap-5 md:flex-row">
+        <div className="flex items-center w-full justify-start gap-3 md:gap-4">
           <div className="text-primary">
-            <Cookie size={50} />
+            <Cookie className="h-10 w-10" size={50} />
           </div>
-          <div className="flex font-sans">
+          <div className="flex font-sans text-xs">
             We use cookies to enhance your experience. By continuing to browse,
-            you agree <br /> to our use of cookies
+            you agree to our use of cookies
           </div>
         </div>
-        <div className="flex items-center gap-2">
-          <Button title="Accept All" onClick={() => handleAccept()} />
-          <Button
-            className="border border-white/10 bg-transparent text-white hover:border-primary hover:bg-transparent"
-            title="Reject"
-            onClick={() => setIsOpen(false)}
+        <div className="flex w-full items-center justify-between md:justify-end">
+          <div className="flex items-center gap-2">
+            <Button
+              className="py-2 text-xs font-sans font-bold"
+              title="Accept All"
+              onClick={() => handleAccept()}
+            />
+            <Button
+              className="border font-sans font-bold border-white/10 bg-transparent py-2 text-xs text-white hover:border-primary hover:bg-transparent"
+              title="Reject"
+              onClick={() => setIsOpen(false)}
+            />
+          </div>
+          <Image
+            className="sm:hidden"
+            src="/static/logo.png"
+            width={20}
+            height={20}
+            alt="LNBG London Coin Logo"
           />
         </div>
       </div>
